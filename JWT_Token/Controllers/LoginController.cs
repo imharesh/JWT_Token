1f7f9378ac35379b72ffa3be4edf1573fff4dba6
imharesh <baraiyaharesh2286@gmail.com>
2023-04-26 15:43:56 +0530

    first commit

---
+++ JWT_Token/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using JWT_Token.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWT_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if(user.Username == "admin" && user.Password == "12345")
+            {
+                _user = new Users { Username = "Haresh Baraiya"};
+            }
+            return _user;
+        }
+
+        public string GenerateToken(Users users)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securitykey,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if (user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response= Ok(new { token } );
+            }
+            return response;
+        }
+    }
+}
